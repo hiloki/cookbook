49d1068e7965a33f47ab7d4dc382ac84b0ff22f1
Sota Yamashita <sota.yamashita@gmail.com>
2015-04-10 21:39:34 +0900

    Update share widget script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,22 @@
 /* jshint devel:true */
 console.log('\nMañana venid temprano\nque haremos cosas muy bonitas.\n\nBy Antoni Gaudi\n\n');
+
+!( function(document, window, element) {
+
+  var dHeight			    = 0,
+  		wScrollCurrent	= 0;
+
+  window.addEventListener( 'scroll', function(){
+
+  	dHeight			    = document.body.offsetHeight;
+  	wScrollCurrent	= window.pageYOffset;
+
+    if ( $(window).scrollTop() >= (dHeight - wScrollCurrent) ) {
+      $(element).fadeOut();
+    } else {
+      $(element).fadeIn();
+    }
+
+  });
+
+}( document, window, '.widget__share'));
